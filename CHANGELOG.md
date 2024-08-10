20afd1ce35809190fcdbff7097c7e7bbcd2af056
Prawesh-SY <meetprawesh@hotmail.com>
2024-08-10 13:48:01 +0545

    Update boys name.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
 ## 0.0.1
 
-* TODO: Describe initial release.
+* This was the initial release of my first ever package for a Flutter app I am trying to bulid.
--- lib/sanskrit_names.dart
+++ lib/sanskrit_names.dart
@@ -29,155 +29,81 @@ class SanskritNames {
     'Mimansa: Eager one',
     'Trinabh: Lord Vishnu',
     'Grantham: Easily accessible',
-    /*Divyam: Divine
-
-Niskarsh: Outcome of work
-
-Mukund: Lord Vishnu (freedom giver, precious stone)
-
-Savar: Lord Shiva
-
-Amol: Faithful, king
-
-Diyan: Judge, leader, captain
-
-Ranav: King
-
-Ekam: Unity
-
-Yuvan: Young king, heir apparent
-
-Ekaveer: A unique hero
-
-Ajai: Invincible, no defeat
-
-Ranajay: Victorious
-
-Ahar: Defender
-
-Ekaksh: One eyed Shiva
-
-Hrishikesh: God Vishnu
-
-Jaksh:God of wealth
-
-Tarush: Conqueror
-
-Tavasya: Strength
-
-Kevalya: Aloneness
-
-Jivanshu: Part of life
-
-Gautam: Name of Buddha
-
-Hansin: Universal soul
-
-Malin: Little warrior, strong
-
-Nirbhay: Fearless
-
-Jayesh: Victor
-
-Kavin: Handsome
-
-Pradyut: Shining, illuminated
-
-Pragyan: Wisdom, supreme intelligence
-
-Ahi: Water, sun, cloud, fawn
-
-Abhra: Cloud
-
-Abjit: Conquering water
-
-Ansu: Sun, ray of light
-
-Bater: Gray quail
-
-Baru: Brave
-
-Kriya: Lord venkateswara
-
-Krit: Artist
-
-Nabhij: Lord Brahma
-
-Casa: Blue Jay
-
-Druhyu: One who loves nature
-
-Harit: Green fragrant plant
-
-Hirav: Greenery
-
-Namah: Bowing in Sanskrit
-
-Kaushtubh: Legendary gem
-
-Kshiraj: Moon
-
-Mrig: Rain or deer
-
-Nirvan: Liberated
-
-Neer: Crystal clear water
-
-Nivam: Deep rooted
-
-Parikshit: Son of Abhimanyu
-
-Prabal: Powerful
-
-Mrinal: Lotus
-
-Pujit: Worshipped
-
-Gagan: Sky, the heavens
-
-Nakin: Dwells in heaven
-
-Reyaansh: Part of Lord Vishnu
-
-Ranav: King
-
-Pradhyuma: Son of Krishna
-
-Pranshu: Tall
-
-Prithvik: God Vishnu
-
-Sarvin: God of love
-
-Druhyu: One who loves nature
-
-Mukul: Bud
-
-Nandan: Friend
-
-Ahim: Cloud
-
-Ahar: Activity
-
-Praveka: Activity
-
-Punj: A group
-
-Rigved: A veda in sanskrit
-
-Sarth: Thinking
-
-Udvah: Continuous
-
-Varnit: Explained
-
-Ved: Holy Hindu book
-
-Vihank: Bird in sky
-
-Sharvil: God Krishna
-
-Sakal: Entire */
+    'Divyam: Divine',
+    'Niskarsh: Outcome of work',
+    'Mukund: Lord Vishnu (freedom giver, precious stone)',
+    'Savar: Lord Shiva',
+    'Amol: Faithful, king',
+    'Diyan: Judge, leader, captain',
+    'Ranav: King',
+    'Ekam: Unity',
+    'Yuvan: Young king, heir apparent',
+    'Ekaveer: A unique hero',
+    'Ajai: Invincible, no defeat',
+    'Ranajay: Victorious',
+    'Ahar: Defender',
+    'Ekaksh: One eyed Shiva',
+    'Hrishikesh: God Vishnu',
+    'Jaksh:God of wealth',
+    'Tarush: Conqueror',
+    'Tavasya: Strength',
+    'Kevalya: Aloneness',
+    'Jivanshu: Part of life',
+    'Gautam: Name of Buddha',
+    'Hansin: Universal soul',
+    'Malin: Little warrior, strong',
+    'Nirbhay: Fearless',
+    'Jayesh: Victor',
+    'Kavin: Handsome',
+    'Pradyut: Shining, illuminated',
+    'Pragyan: Wisdom, supreme intelligence',
+    'Ahi: Water, sun, cloud, fawn',
+    'Abhra: Cloud',
+    'Abjit: Conquering water',
+    'Ansu: Sun, ray of light',
+    'Bater: Gray quail',
+    'Baru: Brave',
+    'Kriya: Lord venkateswara',
+    'Krit: Artist',
+    'Nabhij: Lord Brahma',
+    'Casa: Blue Jay',
+    'Druhyu: One who loves nature',
+    'Harit: Green fragrant plant',
+    'Hirav: Greenery',
+    'Namah: Bowing in Sanskrit',
+    'Kaushtubh: Legendary gem',
+    'Kshiraj: Moon',
+    'Mrig: Rain or deer',
+    'Nirvan: Liberated',
+    'Neer: Crystal clear water',
+    'Nivam: Deep rooted',
+    'Parikshit: Son of Abhimanyu',
+    'Prabal: Powerful',
+    'Mrinal: Lotus',
+    'Pujit: Worshipped',
+    'Gagan: Sky, the heavens',
+    'Nakin: Dwells in heaven',
+    'Reyaansh: Part of Lord Vishnu',
+    'Ranav: King',
+    'Pradhyuma: Son of Krishna',
+    'Pranshu: Tall',
+    'Prithvik: God Vishnu',
+    'Sarvin: God of love',
+    'Druhyu: One who loves nature',
+    'Mukul: Bud',
+    'Nandan: Friend',
+    'Ahim: Cloud',
+    'Ahar: Activity',
+    'Praveka: Activity',
+    'Punj: A group',
+    'Rigved: A veda in sanskrit',
+    'Sarth: Thinking',
+    'Udvah: Continuous',
+    'Varnit: Explained',
+    'Ved: Holy Hindu book',
+    'Vihank: Bird in sky',
+    'Sharvil: God Krishna',
+    'Sakal: Entire',
     // Add more boy's name here
   ];
   static const List<String> girlName = [
